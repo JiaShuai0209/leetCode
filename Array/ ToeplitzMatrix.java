7629f213508bc834c7e1ec4c1015252caa69b21a
ssJ <jiashuai0209@gmail.com>
2018-05-04 15:38:51 +0800

    Update and rename xxxx.java to  ToeplitzMatrix.java

---
+++ Array/ ToeplitzMatrix.java
@@ -0,0 +1,15 @@
+//二维数组的长度判断
+class Solution {
+    public boolean isToeplitzMatrix(int[][] matrix) {
+        boolean flag = true;
+        for(int i = 0; i < matrix.length-1; i++){
+            for(int j = 0; j < matrix[i].length-1; j++){
+               // if(i < 19 & j <19){
+                  if(matrix[i][j] != matrix[i+1][j+1]) 
+                      flag = false;
+                //}
+            }
+        }
+        return flag;
+    }
+}
